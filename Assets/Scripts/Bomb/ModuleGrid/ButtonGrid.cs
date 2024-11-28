585ead938889e55c523e32a84ef47d90b42f214d
4o3aXPEHb <polunosik3@gmail.com>
2024-11-28 18:08:24 +0400

    джойстики нажимают на кнопки

--- Assets/Scripts/Bomb/ModuleGrid/ButtonGrid.cs
+++ Assets/Scripts/Bomb/ModuleGrid/ButtonGrid.cs
@@ -19,7 +19,7 @@ public class ButtonGrid : MonoBehaviour
         
     }
 
-    private void OnMouseDown()
+    public void OnMouseDown()
     {
         module.Move(horizontalDirection, verticalDirection);
     }
